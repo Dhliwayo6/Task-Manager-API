4f1c4683919aff2254c6925050368509c66f633c
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-06-19 14:50:32 +0200

    sout

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
+
+		System.out.println("Task-Manager-API");
 		SpringApplication.run(TaskManagerApplication.class, args);
 	}
 
