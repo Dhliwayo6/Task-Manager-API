c55c55c39234c68032eec98c769d1c5a9a49f011
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-08-25 19:01:19 +0200

    Update UserController to include authenticated user path

--- src/main/java/com/example/taskmanager/controller/UserController.java
+++ src/main/java/com/example/taskmanager/controller/UserController.java
@@ -4,31 +4,50 @@ import com.example.taskmanager.model.UpdateUserCommand;
 import com.example.taskmanager.model.User;
 import com.example.taskmanager.model.UserDTO;
 import com.example.taskmanager.services.user.*;
+import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RequestMapping("/users")
 @RestController
 public class UserController {
 
+    private final UserService userService;
     private final CreateUserService createUserService;
     private final GetUserService getUserService;
     private final GetUsersService getUsersService;
     private final DeleteUserService deleteUserService;
     private final UpdateUserService updateUserService;
 
-
     public UserController(CreateUserService createUserService,
                           GetUserService getUserService,
                           GetUsersService getUsersService,
                           DeleteUserService deleteUserService,
-                          UpdateUserService updateUserService) {
+                          UpdateUserService updateUserService,
+                          UserService userService) {
         this.createUserService = createUserService;
         this.getUserService = getUserService;
         this.getUsersService = getUsersService;
         this.deleteUserService = deleteUserService;
         this.updateUserService = updateUserService;
+        this.userService = userService;
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<User> authenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User currentuser = (User) authentication.getPrincipal();
+        return ResponseEntity.ok(currentuser);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> users() {
+        List<User> users = userService.allUsers();
+        return ResponseEntity.ok(users);
     }
 
     @PostMapping("/user")
@@ -40,8 +59,6 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> getUsers(){
         return getUsersService.execute(null);
     }
-
-    //new get mapping to find by id
 
     @GetMapping("/user/{id}")
     public ResponseEntity<UserDTO> getUserById(@PathVariable Integer id) {
