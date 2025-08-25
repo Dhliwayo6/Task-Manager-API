e22da8efe8310e8be2415dee3445191b8e781a1d
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-08-25 13:04:42 +0200

    Update UserRepository to extend CrudRepository, update interface methods to findByEmail and findByVerificationCode

--- src/main/java/com/example/taskmanager/repository/UserRepository.java
+++ src/main/java/com/example/taskmanager/repository/UserRepository.java
@@ -3,11 +3,12 @@ package com.example.taskmanager.repository;
 import com.example.taskmanager.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
 
-    @Query("SELECT u FROM User u WHERE u.username = ?1")
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
 }
