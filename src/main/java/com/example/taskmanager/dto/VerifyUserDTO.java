39bdd7d719b958e6bee50f8b966f91360dfb0759
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-08-25 13:01:15 +0200

    Add VerifyUserDTO

--- src/main/java/com/example/taskmanager/dto/VerifyUserDTO.java
+++ src/main/java/com/example/taskmanager/dto/VerifyUserDTO.java
@@ -1,4 +1,22 @@
 package com.example.taskmanager.dto;
 
 public class VerifyUserDTO {
+    private String email;
+    private String verificationCode;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVerificationCode() {
+        return verificationCode;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
 }
