eff8dbf2e40f2d876578848fa2d41ff36dd82438
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-09-14 16:40:53 +0200

    minor formating

--- src/main/java/com/example/taskmanager/repository/TaskRepository.java
+++ src/main/java/com/example/taskmanager/repository/TaskRepository.java
@@ -2,10 +2,8 @@ package com.example.taskmanager.repository;
 
 import com.example.taskmanager.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends JpaRepository <Task, Integer> {
