bcbd6f42ad4870906c3c95d76cdea0057dfbfc63
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-12 20:02:50 +0200

    Update ErrorMesages enum to include error messages

--- src/main/java/com/example/taskmanager/enums/ErrorMessages.java
+++ src/main/java/com/example/taskmanager/enums/ErrorMessages.java
@@ -1,4 +1,23 @@
 package com.example.taskmanager.enums;
 
 public enum ErrorMessages {
+    USER_NOT_FOUND("User not found!"),
+    ACCOUNT_NOT_VERIFIED("Account not verified, verify account before logging in"),
+    EMAIL_ALREADY_EXISTS("The email you entered already exists"),
+    INVALID_TOKEN("Invalid token"),
+    ACCOUNT_ALREADY_VERIFIED("Account has already been verified"),
+    TASK_NOT_FOUND("Task not found"),
+    VERIFICATION_TOKEN_EXPIRED("The verification token you entered has expired");
+
+    private String message;
+
+    ErrorMessages(String message) {
+        this.message = message;
+    }
+
+    private String getMessage() {
+        return message;
+    }
+
+
 }
