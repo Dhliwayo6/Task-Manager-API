53dad2201844abb47d148ab4ecdbba43e818bf84
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-12 20:29:48 +0200

    Update GlobalExceptionHandler to incorporate custom exceptions

---
+++ src/main/java/com/example/taskmanager/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.example.taskmanager.exceptions;
+
+import com.example.taskmanager.dto.ErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AccountAlreadyVerifiedException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ErrorResponse handleAccountAlreadyFoundException(AccountAlreadyVerifiedException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(AccountNotVerifiedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseBody
+    public ErrorResponse handleAccountNotFoundException(AccountNotVerifiedException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ExceptionHandler(EmailAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ResponseBody
+    public ErrorResponse handleEmailAlreadyExistsException(EmailAlreadyExistsException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+}
