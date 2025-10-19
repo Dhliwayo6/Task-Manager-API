0d6145ced8fd2db784069f5860cd92d9cf0649d1
tachitujhb024 <tachitujhb024@student.wethinkcode.co.za>
2025-10-19 02:03:35 +0200

    Add password and email validation to Register user DTO

--- src/main/java/com/example/taskmanager/dto/RegisterUserDTO.java
+++ src/main/java/com/example/taskmanager/dto/RegisterUserDTO.java
@@ -1,31 +1,24 @@
 package com.example.taskmanager.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class RegisterUserDTO {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            message = "Password must have an uppercase letter, a lowercase letter, a digit and at least one special character")
     private String password;
     private String username;
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 }
